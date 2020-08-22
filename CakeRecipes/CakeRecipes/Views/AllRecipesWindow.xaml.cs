5512bfb2dbd8f7ef7e56f21120f348cff8400393
kris994 <kristipomo@gmail.com>
2020-08-22 22:30:38 +0200

    Fixed merging error

--- CakeRecipes/CakeRecipes/Views/AllRecipesWindow.xaml.cs
+++ CakeRecipes/CakeRecipes/Views/AllRecipesWindow.xaml.cs
@@ -2,8 +2,10 @@
 using CakeRecipes.ViewModel;
 using System;
 using System.Linq;
+using System.Media;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CakeRecipes.Views
 {
@@ -25,7 +27,64 @@ namespace CakeRecipes.Views
                 btnAddRecipe.Margin = new Thickness(30, 0, 140, 15);
 
             }
+        }
 
+        private void TxtBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = e.Key == Key.Space;
+            if (e.Key == Key.Space)
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private Boolean TextAllowed(String s)
+        {
+            foreach (Char c in s.ToCharArray())
+            {
+                if (Char.IsLetter(c) || Char.IsControl(c))
+                {
+
+                    continue;
+                }
+                else
+                {
+                    SystemSounds.Beep.Play();
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // zabranjuje da se kopiraju brojevi u polje
+        private void PastingHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            String s = (String)e.DataObject.GetData(typeof(String));
+            if (!TextAllowed(s)) e.CancelCommand();
+        }
+
+        //only numbers
+        private void PreviewNumberInputHandler(Object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !NumberAllowed(e.Text);
+        }
+
+        private Boolean NumberAllowed(String s)
+        {
+            foreach (Char c in s.ToCharArray())
+            {
+                if (Char.IsDigit(c) || Char.IsControl(c))
+                {
+
+                    continue;
+                }
+                else
+                {
+                    SystemSounds.Beep.Play();
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
